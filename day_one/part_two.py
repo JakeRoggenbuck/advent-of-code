8c4c72973a247b46ab1166502c24b6241fd7cccb
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-12-01 13:21:56 -0800

    Add day one

---
+++ day_one/part_two.py
@@ -0,0 +1,14 @@
+from jakesutils.config import Config
+
+
+day_one_input = Config("./input", "txt").config.split()
+day_one_input = [int(num) for num in day_one_input]
+
+for first_num in day_one_input:
+    for second_num in day_one_input:
+        for third_num in day_one_input:
+            if first_num + second_num + third_num == 2020:
+                print(f"{first_num} + {second_num} + {third_num} = {2020}")
+                multi = first_num * second_num * third_num
+                print(f"{first_num} * {second_num} * {third_num} = {multi}")
+                exit()
