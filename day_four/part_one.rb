facc34d925c6378b3b58a17223e81df5be9c75cb
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-12-04 21:51:37 -0800

    Add part one in ruby from crystal file

---
+++ day_four/part_one.rb
@@ -0,0 +1,34 @@
+class Passport
+	def initialize(raw_passport)
+		@raw_passport = raw_passport
+		@parsed_passport = parse_passport()
+	end
+
+	def parse_passport
+		parsed_passport = {}
+		(@raw_passport.split()).each() do |item|
+			key, value = item.split(":")
+			parsed_passport[key] = value
+		end
+		return parsed_passport
+	end
+
+	def check
+		if @parsed_passport.size == 8
+			return true
+		end
+		if @parsed_passport.size == 7 && ! @parsed_passport.has_key?("cid")
+			return true
+		end
+	end
+end
+
+
+valid = 0
+File.read("./input").split("\n\n").each do |raw_line|
+	passport = Passport.new raw_line
+  	if passport.check()
+    	valid += 1
+  	end
+end
+puts valid
