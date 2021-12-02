3c07e9c20d7cab5acb4732109960cd4663fda3f8
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-02 10:37:19 -0800

    Add day one

--- 2021/2/python/day_one.py
+++ 2021/2/python/day_one.py
@@ -1,25 +1,30 @@
-with open("../input") as file:
-    lines = file.readlines()
+def non_golf():
+    with open("../input") as file:
+        lines = file.readlines()
 
-number = 0
+    number = 0
 
-field = { "forward": 1, "up": 1j, "down": -1j}
+    field = {"forward": 1, "up": 1j, "down": -1j}
 
-for line in lines:
-    word, num = line.split(" ")
-    number += field[word] * int(num)
+    for line in lines:
+        word, num = line.split(" ")
+        number += field[word] * int(num)
 
-number = int(abs(number.imag) * number.real)
-print(number)
+    number = int(abs(number.imag) * number.real)
+    print(number)
+
 
 def golf():
     f=open("../input")
     l=f.readlines()
     n=0
-    d={"forward":1,"up":1j,"down":-1j}
+    d={"f":1,"u":1j,"d":-1j}
     for a in l:
         w,m=a.split(" ")
-        n+=d[w]*int(m)
+        n+=d[w[0]]*int(m)
     n=int(abs(n.imag)*n.real)
     print(n)
     f.close()
+
+if __name__ == "__main__":
+    golf()
