483cd702acf0d2caaddc44298114c1f7de324604
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-04 19:22:18 -0800

    Add comments

--- 2021/2/python/part_two.py
+++ 2021/2/python/part_two.py
@@ -3,17 +3,30 @@ def list_comp():
     with open("../input") as file:
         lines = file.readlines()
 
+    # Lists can be changed as side effects in function without that variable in
+    # scope, so this works but n = 0 would not
     n = [0]
 
     def field(x, b):
+        # Change the value of n based on if it should be negative or positive
         n[0] += {"u": -1, "d": 1}[x[0]] * b
 
+    # Add up all of the complex numbers
     number = sum(
         list(
+            # Remove all of the None
             filter(
                 lambda x: x is not None,
                 [
+                    # The value is part real and part imaginary
+                    # b is the real part, (b * n[0]) * 1j is imaginary because
+                    # we we multiply by 1j
+
+                    # field causes a side effect that changes the value of n
+                    # because it should not be added up in the sum but we still
+                    # need to keep track of it, so intentional side effects
                     b + (b * n[0]) * 1j if a[0] == "f" else field(a, b)
+                    # For each line, split it, and make the second value an int
                     for (a, b) in [(c, int(d)) for (c, d) in [x.split() for x in lines]]
                 ],
             )
