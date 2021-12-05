8e118354e567259f05f443a12b90ec533feb9c8d
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-12-05 11:45:37 -0800

    Make part one a little nicer

--- 2021/1/python/part_one.py
+++ 2021/1/python/part_one.py
@@ -3,14 +3,10 @@ with open("../input") as file:
 
 last = lines[0]
 num_increases = 0
-print(f"{last} (N/A - no previous measurement)")
 
 for x in lines[1:]:
     if x > last:
         num_increases += 1
-        print(f"{x} (increased)")
-    else:
-        print(f"{x} (decreased)")
     last = x
 
 print(num_increases)
