f02c95efba89945ec894844371ddab2d620b707c
Jake <jakeroggenbuck2@gmail.com>
2024-12-07 18:24:38 -0800

    Add part two

--- 2024/01/typescript/main.ts
+++ 2024/01/typescript/main.ts
@@ -9,8 +9,7 @@ function part_one(text: string): Number {
   for (let i = 0; i < lines.length; i++) {
     const [a, b] = lines[i].split('  ');
 
-    if (lines[i] !== "") {
-
+    if (lines[i] !== '') {
       left.push(Number(a));
       right.push(Number(b));
     }
@@ -19,12 +18,33 @@ function part_one(text: string): Number {
   left.sort();
   right.sort();
 
-  for (let i = 0; i < lines.length; i++) {
+  for (let i = 0; i < left.length; i++) {
     let c = left[i] - right[i];
+    total += Math.abs(c);
+  }
 
-    if (!Number.isNaN(c)) {
-      total += Math.abs(c);
+  return total;
+}
+
+function part_two(text: string): Number {
+  const lines = text.split('\n');
+
+  let total = 0;
+
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b] = lines[i].split('  ');
+
+    if (lines[i] !== '') {
+      left.push(Number(a));
+      right.push(Number(b));
     }
+  }
+
+  for (let i = 0; i < left.length; i++) {
+    total += right.filter((x) => x === left[i]).length * left[i];
   }
 
   return total;
@@ -35,4 +55,7 @@ if (import.meta.main) {
 
   const p1 = part_one(text);
   console.log(p1);
+
+  const p2 = part_two(text);
+  console.log(p2);
 }
