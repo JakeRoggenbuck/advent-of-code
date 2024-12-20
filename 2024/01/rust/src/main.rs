1bf564c3f601ca5453688d12a7a5dd8817b3de38
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2024-12-20 00:46:30 -0800

    Add rust version complete

--- 2024/01/rust/src/main.rs
+++ 2024/01/rust/src/main.rs
@@ -1,18 +1,23 @@
 use std::fs::read_to_string;
+use std::iter::zip;
 
 fn main() {
-    let file = read_to_string("../../input").expect("read");
+    let file = read_to_string("../../input").expect("Should have read file.");
 
     let lines = file.split("\n");
-    let mut left: Vec<&str> = Vec::with_capacity(1000);
-    let mut right: Vec<&str> = Vec::with_capacity(1000);
+    let mut left: Vec<i32> = Vec::with_capacity(1000);
+    let mut right: Vec<i32> = Vec::with_capacity(1000);
 
     for line in lines {
         if let Some((l, r)) = line.split_once("   ") {
-            left.push(l);
-            right.push(r);
+            left.push(l.parse().expect("Correct parse."));
+            right.push(r.parse().expect("Correct parse."));
         }
     }
 
-    println!("{:?} {:?}", left, right);
+    left.sort();
+    right.sort();
+
+    let part_1: i32 = zip(left, right).map(|(a, b)| (a - b).abs()).sum();
+    println!("{part_1}");
 }
