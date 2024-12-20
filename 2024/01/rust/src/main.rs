3cf5d99472bc51c6641f842054acc66549d3a08e
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2024-12-20 00:39:42 -0800

    if let allows to remove lines.pop

--- 2024/01/rust/src/main.rs
+++ 2024/01/rust/src/main.rs
@@ -3,11 +3,9 @@ use std::fs::read_to_string;
 fn main() {
     let file = read_to_string("../../input").expect("read");
 
-    let mut lines: Vec<&str> = file.split("\n").into_iter().collect();
+    let lines = file.split("\n");
     let mut left: Vec<&str> = Vec::with_capacity(1000);
     let mut right: Vec<&str> = Vec::with_capacity(1000);
-
-    lines.pop();
 
     for line in lines {
         if let Some((l, r)) = line.split_once("   ") {
